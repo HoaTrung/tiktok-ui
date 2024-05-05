138cd29cecb166404d0708721dbe7cee875ef9e5
Trung Hoa <duongtrunghoa987@gmail.com>
2024-05-05 11:26:29 +0700

    .

--- src/components/Layout/HeaderOnly/index.js
+++ src/components/Layout/HeaderOnly/index.js
@@ -1,14 +1,11 @@
-import Header from './Header';
+import Header from '~/components/Layout/components/Header';
 
 function DefaultLayout({ children }) {
     return (
         <div>
             <Header />
-            <div className='container'>
-                
-                <div className='content'>
-                    {children}
-                </div>
+            <div className="container">
+                <div className="content">{children}</div>
             </div>
         </div>
     );
